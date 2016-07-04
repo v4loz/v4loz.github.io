1d24b1c939810975d47d4f34f94db17ca263fa7a
mrmonopz <pascalr@openmailbox.org>
2016-07-05 01:31:46 +0200

    test

---
+++ out/amazon.php
@@ -0,0 +1,4 @@
+
+<?php
+      echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://www.amazon.de/gp/product/B01F28DVVO/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=B01F28DVVO&linkCode=as2&tag=infinite_warfare-21" rel="nofollow">';
+?> 
